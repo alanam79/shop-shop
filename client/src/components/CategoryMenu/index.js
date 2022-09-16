104242e6e7cd01cd528c925a080a8fff98545237
Alana <halo2382001@yahoo.com>
2022-09-16 17:43:21 -0400

    finished adding state to page

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import { useQuery } from "@apollo/client";
 import { QUERY_CATEGORIES } from "../../utils/queries";
 import { useStoreContext } from "../../utils/GlobalState";
-
 import {
   UPDATE_CATEGORIES,
   UPDATE_CURRENT_CATEGORY,
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,12 +3,20 @@ import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 
 const Home = () => {
-  const [currentCategory, setCategory] = useState("");
+  // const [currentCategory, setCategory] = useState("");
+
+  //   return (
+  //     <div className="container">
+  //       <CategoryMenu setCategory={setCategory} />
+  //       <ProductList currentCategory={currentCategory} />
+  //     </div>
+  //   );
+  // };
 
   return (
     <div className="container">
-      <CategoryMenu setCategory={setCategory} />
-      <ProductList currentCategory={currentCategory} />
+      <CategoryMenu />
+      <ProductList />
     </div>
   );
 };
