96d1ba704258b87ed616f91a89af566d43f452f1
Alana <halo2382001@yahoo.com>
2022-09-19 18:27:43 -0400

    finished creating and testing the shopping cart

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -7,7 +7,8 @@ import { TOGGLE_CART } from "../../utils/actions";
 
 const Cart = () => {
   const [state, dispatch] = useStoreContext();
-
+  // console log to verify this is, in fact, receiving the state updates
+  // console.log(state);
   function toggleCart() {
     dispatch({ type: TOGGLE_CART });
   }
@@ -15,11 +16,20 @@ const Cart = () => {
   if (!state.cartOpen) {
     return (
       <div className="cart-closed" onClick={toggleCart}>
-        <span
-          role="img"
-          aria-label="trash">🛒</span>
+        <span role="img" aria-label="trash">
+          🛒
+        </span>
       </div>
     );
+  }
+
+  // This function will add up the prices of everything saved in state.cart, which can then be displayed in the JSX
+  function calculateTotal() {
+    let sum = 0;
+    state.cart.forEach((item) => {
+      sum += item.price * item.purchaseQuantity;
+    });
+    return sum.toFixed(2);
   }
 
   return (
@@ -28,35 +38,68 @@ const Cart = () => {
         [close]
       </div>
       <h2>Shopping Cart</h2>
-      <div>
-        <CartItem
-          item={{
-            name: "Camera",
-            image: "camera.jpg",
-            price: 5,
-            purchaseQuantity: 3,
-          }}
-        />
-        <CartItem
-          item={{
-            name: "Soap",
-            image: "soap.jpg",
-            price: 6,
-            purchaseQuantity: 4,
-          }}
-        />
-
-        <div className="flex-row space-between">
-          <strong>Total: $0</strong>
-          {Auth.loggedIn() ? (
-            <button>Checkout</button>
-          ) : (
-            <span>(log in to check out</span>
-          )}
+      {state.cart.length ? (
+        <div>
+          {state.cart.map((item) => (
+            <CartItem key={item._id} item={item} />
+          ))}
+          <div className="flex-row space-between">
+            <strong>Total: ${calculateTotal()}</strong>
+            {Auth.loggedIn() ? (
+              <button>Checkout</button>
+            ) : (
+              <span>(log in to check out)</span>
+            )}
+          </div>
         </div>
-      </div>
+      ) : (
+        <h3>
+          <span role="img" aria-label="shocked">
+            😱
+          </span>
+          You haven't added anything to your cart yet!
+        </h3>
+      )}
     </div>
   );
 };
+
+// INITIAL RETURN STATEMENT WITH HARD CODED ITEMS IN BASKET FOR TESTING
+//   return (
+//     <div className="cart">
+//       <div className="close" onClick={toggleCart}>
+//         [close]
+//       </div>
+//       <h2>Shopping Cart</h2>
+//       <div>
+//         <CartItem
+//           item={{
+//             name: "Camera",
+//             image: "camera.jpg",
+//             price: 5,
+//             purchaseQuantity: 3,
+//           }}
+//         />
+//         <CartItem
+//           item={{
+//             name: "Soap",
+//             image: "soap.jpg",
+//             price: 6,
+//             purchaseQuantity: 4,
+//           }}
+//         />
+
+//         <div className="flex-row space-between">
+//           <strong>Total: $0</strong>
+//           {Auth.loggedIn() ? (
+//             <button>Checkout</button>
+//           ) : (
+//             <span>(log in to check out</span>
+//           )}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
 export default Cart;
--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,6 +1,36 @@
 import React from "react";
+import { useStoreContext } from "../../utils/GlobalState";
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 
 const CartItem = ({ item }) => {
+  // we only destructured the dispatch() function from the useStoreContext Hook, because the CartItem component has no need to read state.
+  const [, dispatch] = useStoreContext();
+
+  const removeFromCart = (item) => {
+    dispatch({
+      type: REMOVE_FROM_CART,
+      _id: item._id,
+    });
+  };
+
+  // allow users to manually edit the quantity of shopping cart items by typing directly in the <input> elements
+  const onChange = (e) => {
+    const value = e.target.value;
+
+    if (value === "0") {
+      dispatch({
+        type: REMOVE_FROM_CART,
+        _id: item._id,
+      });
+    } else {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: item._id,
+        purchaseQuantity: parseInt(value),
+      });
+    }
+  };
+
   return (
     <div className="flex-row">
       <div>
@@ -12,8 +42,16 @@ const CartItem = ({ item }) => {
         </div>
         <div>
           <span>Qty:</span>
-          <input type="number" placeholder="1" value={item.purchaseQuantity} />
-          <span role="img" aria-label="trash">
+          <input
+            type="number"
+            placeholder="1"
+            value={item.purchaseQuantity}
+            onChange={onChange}
+          />
+          <span
+            role="img"
+            aria-label="trash"
+            onClick={() => removeFromCart(item)}>
             🗑️
           </span>
         </div>
